8b4c2e5c1b663e3fa644c21e030b4345b52d19f5
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-11 16:33:47 -0700

    useForm and useLocalStorage custom hooks added

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useForm } from '../hooks/useForm';
 import './Styles.css';
 
 export default function SignUp() {
-    return (
-        <h1>Sign Up</h1>
+
+    const [values, handleChanges, clearForm] = useForm('newClient',
+        {
+            username:'',
+            email:''
+        }
+    );
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        alert(values.username);
+        clearForm(e);
+    };
+
+    return ( 
+        <> 
+            <h1>Sign Up</h1> 
+            <form onSubmit={ handleSubmit } >
+                <fieldset>
+                    <legend>Add New Client</legend>
+                    <input type="text" value={ values.username } onChange={ handleChanges } name="username"/>
+                    <input type="text" value={ values.email } onChange={ handleChanges } name="email"/>
+                    <button>Clear</button> <button>Submit</button>S
+                </fieldset>
+            </form>
+        </>
     )
 }
