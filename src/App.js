979c86e4651ba1ced9cfd809de54b86d8b6d50d8
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-11 13:34:53 -0700

    users route removed

--- src/App.js
+++ src/App.js
@@ -49,9 +49,6 @@ export default function App() {
           <Route path="/about">
             <About />
           </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
           <Route path="/">
             <Home />
           </Route>
